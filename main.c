6f879cdc63ede5e36707884baf6d9a58c7889e53
ALTracer <tolstov_den@mail.ru>
2022-10-20 23:32:19 +0300

    Template of helloworld in C

---
+++ main.c
@@ -0,0 +1,16 @@
+/* ============================================================================
+ * Name : main.c
+ * Author : @ALTracer
+ * Copyright : (c) 2022 All rights reserved.
+ * Description : Prints integers formatted in hexadecimal.
+ * ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	if (argc < 2) {	printf("I am %s with %d argc\n", argv[0], argc); }
+
+	return 0;
+}
